d2589bc34e805c67783eaa331230391e04dfba76
Ryan O'Neill <ryanponeill00@gmail.com>
2022-02-23 12:14:02 -0500

    commit tree d90b7e8014bea249bdd419817559bd1bbb676cae

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+require 'pry'
+def countdown(counter)
 
-def countdown
+    while counter > 0
+        puts "#{counter} SECOND(S)!"
+        counter -= 1
+    end
+    if counter = 0
+        return "HAPPY NEW YEAR!"
+    end
+end
+
+def countdown_with_sleep(timer)
+    sleep(timer)
 end
